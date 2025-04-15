29b08725a3a8240ed54f01c2d1f9941deebeb50b
adrieeltorres1 <adrieltorreslds@gmail.com>
2025-04-15 14:55:44 -0300

    Atualização da página bnc dados

--- pages/BancoDeDados.jsx
+++ pages/BancoDeDados.jsx
@@ -1,7 +1,80 @@
+// const BancoDeDados = () => {
+//   return (
+//     <>
+
+//       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6  bg-gray-100 min-h-screen items-center">
+
+//         <div className="bg-cyan-950 w-[350px] h-[250px] rounded-lg text-white shadow-md">
+//           <h2 className="flex justify-center text-xl font-bold mb-4 mt-5">Aula 1</h2>
+//           <div className="flex justify-center mt-28">
+//             <button
+//               className=" px-6 py-3 bg-vermelho-danger text-white font-medium rounded-lg ">Baixar PTT
+//             </button>
+//           </div>
+//         </div>
+
+//         <div className="bg-cyan-950 w-[350px] h-[250px]  rounded-lg text-white shadow-md">
+//           <h2 className="flex justify-center text-xl font-bold mb-4 mt-5">Aula 2</h2>
+//           <div className="flex justify-center mt-28" >
+//             <button
+//               className=" px-6 py-3 bg-vermelho-danger text-white font-medium rounded-lg">Baixar PTT</button>
+//           </div>
+//         </div>
+
+//         <div className="bg-cyan-950 w-[350px] h-[250px]  rounded-lg text-white shadow-md">
+//           <h2 className="flex justify-center text-xl font-bold mb-4 mt-5">Aula 3</h2>
+//           <div className="flex justify-center mt-28" >
+//             <button
+//               className=" px-6 py-3 bg-vermelho-danger text-white font-medium rounded-lg">Baixar PTT</button>
+//           </div>
+//         </div>
+
+//         <div className="bg-cyan-950 w-[350px] h-[250px]  rounded-lg text-white shadow-md">
+//           <h2 className="flex justify-center text-xl font-bold mb-4 mt-5">Aula 4</h2>
+//           <div className="flex justify-center mt-28" >
+//             <button
+//               className=" px-6 py-3 bg-vermelho-danger text-white font-medium rounded-lg">Baixar PTT</button>
+//           </div>
+//         </div>
+
+//         <div className="bg-cyan-950 w-[350px] h-[250px] rounded-lg text-white shadow-md">
+//           <h2 className="flex justify-center text-xl font-bold mb-4 mt-5">Aula 5</h2>
+//           <div className="flex justify-center mt-28">
+//             <button
+//               className=" px-6 py-3 bg-vermelho-danger text-white font-medium rounded-lg">Baixar PTT
+//             </button>
+//           </div>
+//         </div>
+
+//         <div className="bg-cyan-950 w-[350px] h-[250px] rounded-lg text-white shadow-md">
+//           <h2 className="flex justify-center text-xl font-bold mb-4 mt-5">Aula 6</h2>
+//           <div className="flex justify-center mt-28">
+//             <button
+//               className=" px-6 py-3 bg-vermelho-danger text-white font-medium rounded-lg">Baixar PTT
+//             </button>
+//           </div>
+//         </div>
+
+//         <div className="flex m-auto">
+//           <button
+//             className=" w-[200px] h-[40px] bg-vermelho-danger text-white font-medium rounded-full">Voltar
+//           </button>
+//         </div>
+//       </div>
+//     </>
+
+//   );
+// }
+
+// export default BancoDeDados;
+
+
 const BancoDeDados = () => {
   return (
     <>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6 ml-20 bg-gray-100 min-h-screen">
+
+      <div className="flex flex-wrap gap-6 justify-center items-center p-6 bg-gray-100 min-h-screen">
+
         <div className="bg-cyan-950 w-[350px] h-[250px] rounded-lg text-white shadow-md">
           <h2 className="flex justify-center text-xl font-bold mb-4 mt-5">Aula 1</h2>
           <div className="flex justify-center mt-28">
@@ -18,6 +91,7 @@ const BancoDeDados = () => {
               className=" px-6 py-3 bg-vermelho-danger text-white font-medium rounded-lg">Baixar PTT</button>
           </div>
         </div>
+
         <div className="bg-cyan-950 w-[350px] h-[250px]  rounded-lg text-white shadow-md">
           <h2 className="flex justify-center text-xl font-bold mb-4 mt-5">Aula 3</h2>
           <div className="flex justify-center mt-28" >
@@ -25,6 +99,7 @@ const BancoDeDados = () => {
               className=" px-6 py-3 bg-vermelho-danger text-white font-medium rounded-lg">Baixar PTT</button>
           </div>
         </div>
+
         <div className="bg-cyan-950 w-[350px] h-[250px]  rounded-lg text-white shadow-md">
           <h2 className="flex justify-center text-xl font-bold mb-4 mt-5">Aula 4</h2>
           <div className="flex justify-center mt-28" >
@@ -32,6 +107,7 @@ const BancoDeDados = () => {
               className=" px-6 py-3 bg-vermelho-danger text-white font-medium rounded-lg">Baixar PTT</button>
           </div>
         </div>
+
         <div className="bg-cyan-950 w-[350px] h-[250px] rounded-lg text-white shadow-md">
           <h2 className="flex justify-center text-xl font-bold mb-4 mt-5">Aula 5</h2>
           <div className="flex justify-center mt-28">
@@ -40,6 +116,7 @@ const BancoDeDados = () => {
             </button>
           </div>
         </div>
+
         <div className="bg-cyan-950 w-[350px] h-[250px] rounded-lg text-white shadow-md">
           <h2 className="flex justify-center text-xl font-bold mb-4 mt-5">Aula 6</h2>
           <div className="flex justify-center mt-28">
@@ -48,6 +125,12 @@ const BancoDeDados = () => {
             </button>
           </div>
         </div>
+
+        <div className="flex justify-center w-full">
+          <button
+            className=" w-[200px] h-[40px] bg-vermelho-danger text-white font-medium rounded-full">Voltar
+          </button>
+        </div>
       </div>
     </>
 
@@ -55,7 +138,5 @@ const BancoDeDados = () => {
 }
 
 export default BancoDeDados;
-
-
 
 
