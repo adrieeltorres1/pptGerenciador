1c323058dc4212251b61e846e97ddd6c1173d0ca
adrieeltorres1 <adrieltorreslds@gmail.com>
2025-04-28 00:22:08 -0300

    fix bug; atualizando page_inicial

--- src/components/PaginaInicial.jsx
+++ src/components/PaginaInicial.jsx
@@ -1,98 +1,3 @@
-// import { useNavigate } from "react-router-dom";
-
-// const PaginaInicial = () => {
-//     const navigate = useNavigate();
-//     return (
-//         <>
-//             <div
-//                 className="min-h-screen"
-//                 style={{
-//                     background: "linear-gradient(to bottom, #fdfdfd, #354ae4)",
-//                 }}
-//             >
-//                 <img src="../src/assets/INBEClogo.png" className="w-[300px] flex items-center" />
-//                 <h2 className="text-[#1d1e46] text-center text-4xl py-2 font-bold">GERENCIADOR DE PTTs</h2>
-//                 <div className="container mx-auto py-5">
-//                     <div className="flex justify-center gap-10">
-
-//                         <div
-//                             className="bg-[#1d1e46] shadow-md rounded-lg text-center text-white hover:bg-[#0d0e31] transition-all duration-300"
-//                             style={{ width: "282px", height: "313.95px" }}
-//                         >
-//                             <p className="text-lg font-medium mt-10">Arquitetura de S.O</p>
-//                             <button className="mt-44 px-4 py-2 w-[200px] bg-white text-[#1d1e46] rounded-lg hover:bg-[#79121f] hover:text-white transition-all duration-300"
-//                             >
-//                                 Buscar
-//                             </button>
-//                         </div>
-//                         <div
-//                             className="bg-[#1d1e46] shadow-md rounded-lg text-center text-white hover:bg-[#0d0e31] transition-all duration-300"
-//                             style={{ width: "282px", height: "313.95px" }}
-//                         >
-//                             <p className="text-lg font-medium mt-10">Segurança da informação</p>
-//                             <button className="mt-44 px-4 py-2 w-[200px] bg-white text-[#1d1e46] rounded-lg hover:bg-[#79121f] hover:text-white transition-all duration-300"
-//                             >
-//                                 Buscar
-//                             </button>
-//                         </div>
-//                         <div
-//                             className="bg-[#1d1e46] shadow-md rounded-lg text-center text-white hover:bg-[#0d0e31] transition-all duration-300"
-//                             style={{ width: "282px", height: "313.95px" }}
-//                         >
-//                             <p className="text-lg font-medium mt-10">Projeto Integrador</p>
-//                             <button className="mt-44 px-4 py-2 w-[200px] bg-white text-[#1d1e46] rounded-lg hover:bg-[#79121f] hover:text-white transition-all duration-300"
-//                             >
-//                                 Buscar
-//                             </button>
-//                         </div>
-//                     </div>
-//                     <div className="flex justify-center gap-10 py-5">
-//                         <div
-//                             className="bg-[#1d1e46] shadow-md rounded-lg text-center text-white hover:bg-[#0d0e31] transition-all duration-300"
-//                             style={{ width: "282px", height: "313.95px" }}
-//                         >
-//                             <p className="text-lg font-medium mt-10">Redes de computadores</p>
-//                             <button className="mt-44 px-4 py-2 w-[200px] bg-white text-[#1d1e46] rounded-lg hover:bg-[#79121f] hover:text-white transition-all duration-300"
-//                             >
-//                                 Buscar
-//                             </button>
-//                         </div>
-//                         <div
-//                             className="bg-[#1d1e46] shadow-md rounded-lg text-center text-white hover:bg-[#0d0e31] transition-all duration-300"
-//                             style={{ width: "282px", height: "313.95px" }}
-//                         >
-//                             <p className="text-lg font-medium mt-10">Programação com Python</p>
-//                             <button className="mt-44 px-4 py-2 w-[200px] bg-white text-[#1d1e46] rounded-lg hover:bg-[#79121f] hover:text-white transition-all duration-300"
-//                             >
-//                                 Buscar
-//                             </button>
-//                         </div>
-//                         <div
-//                             className="bg-[#1d1e46] shadow-md rounded-lg text-center text-white hover:bg-[#0d0e31] transition-all duration-300"
-//                             style={{ width: "282px", height: "313.95px" }}
-//                         >
-//                             <p className="text-lg font-medium mt-10">Banco de dados</p>
-//                             <button
-//                                 to="/bancodedados" // Aqui a navegação ocorre declarativamente
-//                                 className="mt-44 px-4 py-2 w-[200px] bg-white text-[#1d1e46] rounded-lg hover:bg-[#79121f] hover:text-white transition-all duration-300"
-//                                 onClick={()=> 
-//                                     navigate("/bancodedados")
-//                                 }
-//                             >
-//                                 Buscar
-
-//                             </button>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </>
-//     );
-// }
-
-// export default PaginaInicial;
-
-
 import { useNavigate } from "react-router-dom";
 import { FaDraftingCompass, FaShieldAlt, FaProjectDiagram, FaNetworkWired, FaPython, FaDatabase } from "react-icons/fa";
 
@@ -107,7 +12,7 @@ const PaginaInicial = () => {
                 }}
             >
                 <div className="flex justify-center py-4">
-                    <img src="../assets/INBEClogo.png" className="w-[300px]" />
+                    <img src="../public/assets/INBEClogo.png" className="w-[300px]" />
                 </div>
 
                 <h2 className="text-[#1d1e46] text-center text-4xl py-2 font-bold">
