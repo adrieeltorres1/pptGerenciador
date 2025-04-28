69c9a001b9be9aa8110dc966195dc6da72d9790f
adrieeltorres1 <adrieltorreslds@gmail.com>
2025-04-28 00:10:03 -0300

    Fix Erro

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router";
-import BancoDeDados from "../pages/bancoDeDados";
+import BancoDeDados from "../pages/BancoDeDados";
 import PaginaInicial from "./components/PaginaInicial";
 
 const App = () => {
