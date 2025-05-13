bbf504f3f56d465035c87fc781f21543504456b7
adrieeltorres1 <adrieltorreslds@gmail.com>
2025-05-13 00:57:58 -0300

    Adicionando arquivo de projeto integrador

--- pages/ProjetoIntegrador.jsx
+++ pages/ProjetoIntegrador.jsx
@@ -7,7 +7,7 @@ const ProjetoIntegrador = () => {
             <h2 className="sm:text-xl text-xl lg:text-3xl font-semibold bg-gray-100 py-8 text-center">Arquivos para estudo. (AV2)</h2>
             <div className="flex flex-wrap gap-6 justify-center items-center p-6 bg-gray-100 min-h-screen">
                 <div className="bg-[#1d1e46] w-[350px] h-[250px] rounded-lg text-white shadow-md">
-                    <h2 className="flex justify-center text-xl font-bold mb-4 mt-5">Pendente aula</h2>
+                    <h2 className="flex justify-center text-xl font-bold mb-4 mt-5">Arquivo para estudo</h2>
                     <div className="flex justify-center mt-28">
                         <button
                             className=" px-6 py-3 bg-vermelho-danger text-white font-medium rounded-lg hover:bg-[#4e151c] transition-all duration-300"
