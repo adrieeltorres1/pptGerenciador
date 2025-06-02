3f4c153326d7e62d147673e3545a6879563cfac3
adrieeltorres1 <adrieltorreslds@gmail.com>
2025-06-01 23:45:59 -0300

    Adicionando arquivos de extensão

--- pages/ProjetoIntegrador.jsx
+++ pages/ProjetoIntegrador.jsx
@@ -21,6 +21,19 @@ const ProjetoIntegrador = () => {
                 </div>
 
                 <div className="bg-[#1d1e46] w-[350px] h-[250px]  rounded-lg text-white shadow-md">
+                    <h2 className="flex justify-center text-xl font-bold mb-4 mt-5">Arquivo para estudo</h2>
+                    <div className="flex justify-center mt-28" >
+                        <button
+                            className=" px-6 py-3 bg-vermelho-danger text-white font-medium rounded-lg hover:bg-[#4e151c]"
+                        onClick={() => {
+                          window.open("https://drive.google.com/uc?export=download&id=189VdbQPHsbvKhBly9cp4dA9XkQb80HJL", "_blank");
+                        }}
+                        >
+                            Baixar PPT
+                        </button>
+                    </div>
+                </div>
+                    <div className="bg-[#1d1e46] w-[350px] h-[250px]  rounded-lg text-white shadow-md">
                     <h2 className="flex justify-center text-xl font-bold mb-4 mt-5">Pendente aula</h2>
                     <div className="flex justify-center mt-28" >
                         <button
